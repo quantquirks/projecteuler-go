13b3824346d460d571b40704c814d1f11503790c
root <root@nukely-dev.localdomain>
2017-08-16 20:01:42 +0000

    removing some console logging

--- 001/solution.go
+++ 001/solution.go
@@ -1,3 +1,5 @@
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
 package main
 
 import (
@@ -35,7 +37,6 @@ func FindProducts(numbers ...int) []int {
 	numbers = append(numbers[:0], numbers[1:]...)
 	for i := 0; i < len(numbers); i++ {
 		products = append(products, starter*numbers[i])
-		fmt.Println(products)
 	}
 	return products
 }
